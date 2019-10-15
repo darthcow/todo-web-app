31c372a5302bf84da9833c71c0ed0388e68f6cc1
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-15 08:34:37 -0300

    added TodosComponent to app. module declarations,FormsModule and HttpClientModule to imports

--- TodoWebApp/src/app/app.module.ts
+++ TodoWebApp/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import {TodosComponent} from './components/todos/todos.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    TodosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
