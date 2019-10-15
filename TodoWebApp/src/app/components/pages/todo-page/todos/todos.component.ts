4eaa1706480470d645bdb7de926d778b714a4284
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-15 10:57:41 -0300

    about and todo-related components to directory

---
+++ TodoWebApp/src/app/components/pages/todo-page/todos/todos.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {Todo} from '../../../../models/Todo';
+import {TodoService} from '../../../../services/todo.service';
+
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  @Input() todoList: Todo[];
+
+  constructor(private todoService: TodoService) {
+    todoService.getTodos();
+  }
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(todos => {this.todoList = todos; });
+  }
+
+  deleteTodo(todo: Todo) {
+console.log('delete me');
+// remove from UI
+this.todoList = this.todoList.filter(t => t.id !== todo.id);
+// Remove from server
+this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo: Todo) {
+this.todoService.addTodo(todo).subscribe();
+this.todoList.unshift(todo);
+  }
+}
