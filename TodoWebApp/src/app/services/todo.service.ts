41fb8d8a270d8a81d2038d789cf68b8b39b06267
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-15 09:54:32 -0300

    limit size of get results and added complete and delete request methods

--- TodoWebApp/src/app/services/todo.service.ts
+++ TodoWebApp/src/app/services/todo.service.ts
@@ -1,18 +1,37 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Todo} from '../models/Todo';
 import {Observable} from 'rxjs';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
   todosUrl = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=20';
 
   constructor(private http: HttpClient) {
   }
 
   getTodos(): Observable<Todo[]> {
-   return this.http.get<Todo[]>(this.todosUrl);
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+
+    return this.http.put(url, todo, httpOptions);
+  }
+
+  deleteTodo(todo: Todo): Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+
+    return this.http.delete<Todo>(url, httpOptions);
+
   }
 }
